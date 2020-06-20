1b218543a7308b8fe3c9d9d44316ad641b47bee0
allanqin <allanqin123@gmail.com>
2020-06-19 18:08:29 -0700

    Initial Commit

---
+++ Test Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Login
+//
+//  Created by Allan Qin on 6/19/20.
+//  Copyright © 2020 Allan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test LoginTests/Test_LoginTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_LoginTests.swift
+//  Test LoginTests
+//
+//  Created by Allan Qin on 6/19/20.
+//  Copyright © 2020 Allan. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Login
+
+class Test_LoginTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
